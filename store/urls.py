1073d06970b856c13de18467a83fd33a1e494953
sid12214737 <uzmir6358@gmail.com>
2025-10-09 17:48:18 +0500

    serializing objects

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('products/', views.product_list)
+    path('products/', views.product_list),
+    path('products/<int:pk>/', views.product_detail),
+    path('collections/<int:pk>/', views.collection_detail, name='collection-detail')
 ]
