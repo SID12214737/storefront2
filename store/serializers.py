eaecdd4790e3df57b3a3b7e866f486c69d6a300a
sid12214737 <uzmir6358@gmail.com>
2025-10-09 18:37:55 +0500

    restful apis for product objects

--- store/serializers.py
+++ store/serializers.py
@@ -2,21 +2,19 @@ from decimal import Decimal
 from rest_framework import serializers
 from .models import *
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
-    taxed_price = serializers.SerializerMethodField('calculate_tax')
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),
-        view_name='collection-detail'
-        )
-
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
+        
+    price_with_tax = serializers.SerializerMethodField('calculate_tax')
+    
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
