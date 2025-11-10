e6e9edd371afc5fd452b3468ebd3cc2f6a5441b6
sid12214737 <uzmir6358@gmail.com>
2025-11-10 10:35:28 +0500

    creating cart item

--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework import status
 from .models import Product, Collection, Review
 from .serializers import *
@@ -54,16 +54,29 @@ class ReviewViewSet(ModelViewSet):
         return {'product_id': self.kwargs['product_pk']}
 
 
-class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+class CartViewSet(CreateModelMixin,
+                  RetrieveModelMixin,
+                  DestroyModelMixin,
+                  GenericViewSet):
+    
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
        
 
 class CartItemViewSet(ModelViewSet):
-    serializer_class = CartItemSerializer
 
-    def get_queryset(self):
-        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
-    
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return AddCartItemSerializer
+        
+        return CartItemSerializer
+
     def get_serializer_context(self):
         return {'cart_id': self.kwargs['cart_pk']}
+
+    def get_queryset(self):
+        return CartItem.objects \
+            .filter(cart_id=self.kwargs['cart_pk']) \
+            .select_related('product')
+    
+    
