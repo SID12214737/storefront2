a44c38d136e78d14f48b9c104e8740114eb8a132
sid12214737 <uzmir6358@gmail.com>
2025-11-12 15:48:01 +0500

    signals

--- core/serializers.py
+++ core/serializers.py
@@ -2,6 +2,8 @@ from djoser.serializers import (UserCreateSerializer as BaseUserCreateSerializer
                                 UserSerializer as BaseUserSerializer)
 from rest_framework import serializers
 
+from store.models import Customer
+
 class UserCreateSerializer(BaseUserCreateSerializer):    
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'username', 'password', 
