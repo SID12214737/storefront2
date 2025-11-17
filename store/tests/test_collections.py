e07133db968525a10bad150640e2e4224ea41fd1
sid12214737 <uzmir6358@gmail.com>
2025-11-17 12:53:09 +0500

    creating automated tests

---
+++ store/tests/test_collections.py
@@ -0,0 +1,14 @@
+from rest_framework import status
+from rest_framework.test import APIClient
+
+class TestCreateCollection:
+    def test_if_user_is_anonymous_return_404(self):
+        # Arrange
+
+        # Act
+        client = APIClient()
+        response = client.post('/store/collections/', {'title': 'a'})
+
+        # Assert
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        
